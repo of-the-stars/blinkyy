28770a9594c129892ffd677c2dca083a9c8d30bb
of-the-stars <stellae.incaelis@gmail.com>
2026-01-14 21:38:39 -0600

    Back on crane, and I'm tantalizingly close to figuring this out

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk/master";
     crane.url = "github:ipetkov/crane";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -17,7 +16,6 @@
       self,
       nixpkgs,
       flake-utils,
-      naersk,
       crane,
       rust-overlay,
     }:
@@ -43,141 +41,120 @@
             );
 
         craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
-        # crane-package = craneLib.buildPackage {
-        #   src = craneLib.cleanCargoSource ./.;
-        #   strictDeps = false;
-        #   doCheck = false;
-        #   cargoArtifacts = if builtins.pathExists ./target then ./target else null;
-        #   buildPhaseCargoCommand = "cargo build --profile release";
-        #   doInstallCargoArtifacts = true;
-        #
-        #   NIX_DEBUG = 5;
-        #
-        #   # cargoArtifacts = null;
-        #   cargoExtraArgs = "--target ${buildTarget}";
-        #
-        #   buildInputs = with pkgs; [
-        #     pkgsCross.avr.buildPackages.gcc
-        #     ravedude
-        #   ];
-        #
-        #   # preBuild = ''
-        #   #   export CARGO_PROFILE_RELEASE_OPT_LEVEL="s"
-        #   #   export CARGO_PROFILE_RELEASE_DEBUG="true"
-        #   #   export CARGO_PROFILE_RELEASE_LTO="true"
-        #   #   export CARGO_PROFILE_RELEASE_PANIC="abort"
-        #   #   export CARGO_PROFILE_RELEASE_CODEGEN_UNITS="1"
-        #   # '';
-        #
-        #   configureCargoCommonVars = '''';
-        #   configurePhase = '''';
-        #   checkPhaseCargoCommand = ''
-        #     echo hiiiiii!!!
-        #   '';
-        #
-        #   preBuild = ''
-        #     # ${pkgs.bat}/bin/bat -pp /build/source/.cargo-home/config.toml
-        #   '';
-        #
-        #   installPhase = ''
-        #     runHook preInstall
-        #
-        #     mkdir -p $out/bin
-        #     cp target/avr-none/release/blinkyy.elf $out/bin
-        #
-        #     runHook postInstall
-        #   '';
-        # };
 
-        # crane-package = craneLib.mkCargoDerivation {
-        #   src = craneLib.cleanCargoSource ./.;
-        #   cargoArtifacts = craneLib.buildDepsOnly rec {
-        #     src = craneLib.cleanCargoSource ./.;
-        #     cargoExtraArgs = "--target ${buildTarget}";
-        #     buildPhaseCargoCommand = ''
-        #       ${cargoBuildCommand} ${cargoExtraArgs}
-        #     '';
-        #     cargoBuildCommand = ''cargo build --release'';
-        #     doCheck = false;
-        #   };
-        #
-        #   checkPhase = '''';
-        #   buildPhaseCargoCommand = ''
-        #     cargo build --release --target avr-none --config ./.cargo/config.toml
-        #   '';
-        # };
-
-        naerskLib = pkgs.callPackage naersk {
-          cargo = rust-toolchain;
-          rustc = rust-toolchain;
+        cleanedSrc = pkgs.lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          filter = path: type: (craneLib.filterCargoSources path type) || (pkgs.lib.hasInfix ".cargo" path);
         };
 
-        naersk-package = naerskLib.buildPackage {
-          src = ./.;
+        commonArgs = rec {
+          src = cleanedSrc;
+          strictDeps = true;
 
-          copyTarget = true;
-          singleStep = true;
-
-          cargoBuildOptions =
-            opts:
-            opts
-            ++ [
-              # "--release"
-              ''--target ${buildTarget}''
-              "-vv"
-              ''--config 'build.rustflags=["-C", "target-cpu=atmega328p"]' ''
-              ''--config profile.release.opt-level=\"s\" ''
-              ''--config profile.release.debug=true''
-              ''--config profile.release.lto=true''
-              ''--config profile.release.codegen-units=1''
-              ''--config profile.release.panic=\"abort\"''
-              "--keep-going"
-            ];
-
-          CARGO_INCREMENTAL = 0;
+          doCheck = false;
 
           buildInputs = with pkgs; [
-            ravedude
-            rust-toolchain
+            pkgsCross.avr.buildPackages.gcc
+            pkgsCross.avr.buildPackages.libc
           ];
 
-          nativeBuildInputs = with pkgs; [
-            pkgsCross.avr.gcc
-          ];
+          cargoExtraArgs = ''
+            --locked -Z build-std=core
+          '';
+          cargoCheckExtraArgs = ''
+            --target ${buildTarget} -Z build-std=core -Z build-std-features=""
+          '';
+
+          buildPhaseCargoCommand = ''
+            cargo build ${cargoExtraArgs}
+          '';
+
+          uselessExtraArgs = ''
+            --config profile.release.panic=\"abort\" --config 'build.rustflags=["-C", "target-cpu=atmega328p", "-C","panic=abort"]' --config 'target.avr-none.rustflags=["-C", "target-cpu=atmega328p", "-C","panic=abort"]'
+          '';
+
+          env = {
+            CARGO_BUILD_TARGET = "${buildTarget}";
+            RUSTCFLAGS = "-C panic=abort";
+            # CARGO_HOME = "/build/source/.cargo";
+            # CARGO_BUILD_INCREMENTAL = "false";
+            # CARGO_LOG = "cargo::core::compiler::fingerprint=trace,cargo_util::paths=trace";
+          };
         };
+
+        cargoArtifacts = craneLib.buildDepsOnly (
+          commonArgs
+          // {
+            dummyBuildrs = pkgs.writeText "build.rs" ''
+              fn main() {
+                // println!("cargo::rustc-env=RUSTFLAGS=-C target-cpu=atmega328p -C panic=abort")
+              }
+            '';
+            dummyrs = pkgs.writeText "dummy.rs" ''
+              #![no_main]
+              #![no_std]
+
+
+              use panic_halt as _;
+
+              #[arduino_hal::entry]
+              fn main() -> ! {
+                loop { }
+              }
+
+              // use core::panic::PanicInfo;
+
+              // #[inline(never)]
+              // #[panic_handler]
+              // fn panic(_info: &PanicInfo) -> ! {
+              //     loop {}
+              // }
+
+              // #[no_mangle]
+              // pub extern "C" fn _start() -> ! {
+              //     loop {}
+              // }
+
+            '';
+          }
+
+        );
+
+        crane-package = craneLib.buildPackage (
+          commonArgs
+          // {
+            # inherit cargoArtifacts;
+          }
+        );
 
       in
       {
         devShells.default = pkgs.mkShell {
           # Inherits buildInputs from crane-package
-          # inputsFrom = [ crane-package ];
+          inputsFrom = [ crane-package ];
 
           # Additional packages
           packages = with pkgs; [
-            pkgsCross.avr.buildPackages.gcc
-            ravedude
             rust-toolchain
             cargo-cache
           ];
 
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          env = {
+            CARGO_BUILD_TARGET = "avr-none";
+            RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
+          };
 
           shellHook = '''';
 
         };
 
         apps.default = flake-utils.lib.mkApp {
-          drv = pkgs.writeShellScriptBin "flash-firmware" ''
+          drv = pkgs.writeShellScriptBin "flash-arduino-uno" ''
             ${pkgs.ravedude}/bin/ravedude -c -b 57600 $out/bin
           '';
-
-          # drv = crane-package;
         };
 
-        packages.default = naersk-package;
-        # packages.default = crane-package;
-
-        # checks = { inherit crane-package; };
+        packages.default = crane-package;
 
         formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
       }
